6c1168ad8abf931ffef9eeb96b515819aa3726b9
김성현 <115062965+oilater@users.noreply.github.com>
2023-07-22 18:37:14 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,20 +12,14 @@ public class Main {
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 		int v = Integer.parseInt(st.nextToken());
-		int count = 0;
-		int temp = 0;
-		if (a == v) {
-			System.out.println(1);
+
+		int day = (v - a) / (a - b);
+		int rest = (v - a) % (a - b);
+
+		if (rest != 0) {
+			System.out.println(day + 2);
 		} else {
-			while (true) {
-				count = v / a;
-				temp += count;
-				v = v - count * a + count * b;
-				if (a >= v) {
-					System.out.println(temp + 1);
-					break;
-				}
-			}
+			System.out.println(day + 1);
 		}
 	}
 }
