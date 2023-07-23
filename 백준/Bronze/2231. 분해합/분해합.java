81c9597229969965b452dd3304fdbd032098065d
김성현 <115062965+oilater@users.noreply.github.com>
2023-07-23 10:06:51 +0900

    [Bronze II] Title: 분해합, Time: 104 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,26 +1,33 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-	static int N;
-
 	public static void main(String[] args) throws IOException {
+ 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		int ans = 0;
-		for (int i = 1; i <= N; i++) {
-			String a = String.valueOf(i);
-			String[] arr = a.split("");
-			int sum = i;
-			for (int j = 0; j < arr.length; j++) {
-				sum += Integer.parseInt(arr[j]);
+        
+		int N = Integer.parseInt(br.readLine());
+        
+		int result = 0;
+
+		for(int i = 0; i < N; i++) {
+			int number = i;
+			int sum = 0;	// 각 자릿수 합 변수 
+			
+			while(number != 0) {
+				sum += number % 10;	// 각 자릿수 더하기
+				number /= 10;
 			}
 			
-			if(sum == N) {
-				ans = i;
+			// i 값과 각 자릿수 누적합이 같을 경우 (생성자를 찾았을 경우) 
+			if(sum + i == N) {
+				result = i;
 				break;
 			}
+			
 		}
-		System.out.println(ans);
+ 
+		System.out.println(result);
 	}
 }
