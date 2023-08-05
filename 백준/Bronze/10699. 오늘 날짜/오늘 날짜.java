82a7ed333eb87d37c4bc90401b1ad21c77d4371a
김성현 <115062965+oilater@users.noreply.github.com>
2023-08-05 18:17:05 +0900

    [Bronze V] Title: 오늘 날짜, Time: 116 ms, Memory: 13320 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		LocalDate date = LocalDate.now();
+		System.out.println(date);
+		
+	}
+}
