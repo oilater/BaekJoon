26354f06af5713c1af21a4ec6f76fedc18cc7189
김성현 <115062965+oilater@users.noreply.github.com>
2023-09-03 17:41:19 +0900

    [Silver II] Title: DFS와 BFS, Time: 240 ms, Memory: 20172 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,69 +1,68 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	static boolean[] visited;
-	static ArrayList<Integer>[] A;
+    static int N, M, V;
+    static List<Integer>[] graph;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) { // 그래프 생성
+            graph[i] = new ArrayList<>(); // 정점은 인덱스 번호다.
+        }
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int start = sc.nextInt();
-		A = new ArrayList[N + 1];
-		// 배열의 각 인덱스에 ArrayList 할당
-		for (int i = 1; i <= N; i++) {
-			A[i] = new ArrayList<Integer>();
-		}
-		// 그래프 완성
-		for (int i = 0; i < M; i++) {
-			int S = sc.nextInt();
-			int E = sc.nextInt();
-			A[S].add(E);
-			A[E].add(S);
-		}
-		// 오름차순 출력을 위해 리스트 정렬하기
-		for (int i = 1; i <= N; i++) {
-			Collections.sort(A[i]);
-		}
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
 
-		visited = new boolean[N + 1];
+        // 정점 번호가 작은 것부터 방문해야 하므로 정렬해준다.
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
 
-		DFS(start);
-		System.out.println();
-		visited = new boolean[N + 1];
-		BFS(start);
-		System.out.println();
+        dfs(V);
+        sb.append('\n');
+        Arrays.fill(visited, false);
+        bfs(V);
 
-	}
+        System.out.println(sb); // 출력
+    }
 
-	private static void BFS(int Node) {
-		Queue<Integer> queue = new LinkedList<Integer>();
-		queue.add(Node);
-		visited[Node] = true;
-		
-		while(!queue.isEmpty()) {
-			int now_Node = queue.poll();
-			System.out.print(now_Node + " ");
-			for (int i : A[now_Node]) {
-				if (!visited[i]) {
-					visited[i] = true;
-					queue.add(i);
-				}
-			}
-		}
-	}
+    private static void dfs(int start) {
+        visited[start] = true;
+        sb.append(start).append(' ');
+        for (int el : graph[start]) {
+            if(!visited[el]) dfs(el);
+        }
+    }
 
-	private static void DFS(int Node) {
-		System.out.print(Node + " ");
-		visited[Node] = true;
-		for (int i : A[Node]) {
-			if (!visited[i]) {
-				DFS(i);
-			}
-		}
-	}
+    private static void bfs(int start) {
+        Queue<Integer> q = new ArrayDeque<>();
+
+        visited[start] = true;
+        q.offer(start);
+        while(!q.isEmpty()) {
+            int el = q.poll();
+            sb.append(el).append(' ');
+            for (int next : graph[el]) {
+                if(!visited[next]) {
+                    visited[next] = true;
+                    q.offer(next);
+                }
+            }
+        }
+    }
 }
