3ffd59cfb1793ad1e0460f4886378040bc656de4
김성현 <115062965+oilater@users.noreply.github.com>
2025-08-10 17:22:06 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 17440 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 const [N, M] = input;
 
-// const N = 4;
-// const M = 2;
 const picked = [];
+const visited = Array.from({length: N}, (_) => false);
 let result = '';
 
 function permutation(depth) {
@@ -13,15 +12,16 @@ function permutation(depth) {
         return;
     }
     
-    // 현재 i가 cur과 같으면 넘겨야 한다
     for (let i = 1; i <= N; i++) {
-        if (picked.includes(i)) continue; // 쓰지 말기
+        if (visited[i]) continue; // 이미 선택했다면 넘김
         picked.push(i); // 일단 i를 넣어
+        visited[i] = true; // 선택 체크
+
         permutation(depth + 1); // 다음건 뽑아야지
 
         picked.pop();
+        visited[i] = false;
     }
-    
 }
 
 permutation(0);
