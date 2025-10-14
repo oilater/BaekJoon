289e342a6de3b3aa86530b0370b81c85992594ae
김성현 <115062965+oilater@users.noreply.github.com>
2025-10-15 01:35:51 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 104 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -1,14 +1,9 @@
 const fs = require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().trim().split('');
 
-const alpha = 'abcdefghijklmnopqrstuvwxyz'.split('');
 const res = Array(26).fill(0);
 
-for (let i = 0; i < input.length; i++) {
-    const c = input[i];
-    for (let j = 0; j < alpha.length; j++) {
-        if (c === alpha[j]) res[j]++;
-    }
+for (const c of input) {
+    ++res[c.charCodeAt(0) - 'a'.charCodeAt(0)];
 }
-
 console.log(...res);
