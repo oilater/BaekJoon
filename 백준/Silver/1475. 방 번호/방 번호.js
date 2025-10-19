1a4f3c122e36d90a86b8e12231f43ab66817f85b
김성현 <115062965+oilater@users.noreply.github.com>
2025-10-19 17:12:28 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const inputs = fs.readFileSync(0).toString().trim().split('');
+
+const arr = Array(10).fill(0);
+
+for (const c of inputs) {
+    if (c === '9' || c === '6') {
+        arr[6] += 0.5;
+    } else {
+        arr[c] += 1;
+    }
+}
+
+console.log(Math.ceil(Math.max(...arr)));
+
